ba2406d85512858c1b3b77a2a53d4087836eb45a
yan <yan.braverman37@gmail.com>
2024-03-31 14:23:55 +0300

    Add Header and set .eslintrc and vite.config.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Component } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+import Header from './components/Header/Header';
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+// import reactLogo from './assets/react.svg';
+// import viteLogo from '/vite.svg';
+import './App.css';
+
+class App extends Component {
+	
+
+	state = {
+		count: 0,
+    movies: ['Gladiator', 'Harry Potter', 'LOTR']
+	};
+
+	render() {
+    console.log(this.state.count)
+		return (
+			<>
+				
+				<div className='card'>
+					<button
+						onClick={() =>
+							this.setState({ count: this.state.count + 1 })
+						}
+					>
+						count is {this.state.count}
+					</button>
+          <Header 
+            count={this.state.count}
+            movies={this.state.movies}
+          />
+					<p>
+						Edit <code>src/App.jsx</code> and save to test HMR
+					</p>
+				</div>
+				<p className='read-the-docs'>
+					Click on the Vite and React logos to learn more
+				</p>
+			</>
+		);
+	}
 }
 
-export default App
+export default App;
